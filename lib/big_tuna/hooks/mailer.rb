d62f5133d24d1f4cbde5a17320185d0c84ad58fc
Marek Rosa <marek.rosa@gmail.com>
2011-10-27 10:42:15 +0200

    Removed duplicit rspec results, add summary

--- lib/big_tuna/hooks/mailer.rb
+++ lib/big_tuna/hooks/mailer.rb
@@ -21,36 +21,6 @@ module BigTuna
       self.append_view_path("lib/big_tuna/hooks")
       default :from => "no-reply@polarion.com"
       
-      def link_to_build(build)
-        @build_url = "http://ci.cloud.polarion.com/builds/#{build.to_param}"
-      end
-
-      def cucumber_build_output(build)
-        doc = Nokogiri::HTML(open(build.output_path('cucumber')))
-        output = ""
-        doc.css('.step.failed').each do |step|
-          output << step.parent.parent.to_s
-        end
-        output
-      end 
-
-      def rspec_build_output(build)
-        doc = Nokogiri::HTML(open(build.output_path('rspec')))
-        output = ""
-        doc.css('dd.failed').each do |step|
-          output << step.parent.parent.to_s
-        end
-        output
-      end
-      
-      def setup_data(build)
-        @build = build
-        @build_url = link_to_build(build)
-        @project = @build.project
-        @cucumber_output = cucumber_build_output(@build)
-        @rspec_output = rspec_build_output(@build)
-      end
-
       def build_failed(build, recipients)
         setup_data(build)
         mail(:to => recipients, :subject => "FAILED build '#{@build.display_name}' in '#{@project.name}'") do |format|
@@ -71,6 +41,45 @@ module BigTuna
           format.html { render "mailer/build_fixed", :layout => 'layout' }
         end
       end
+
+      private 
+      
+      def setup_data(build)
+        @build = build
+        @build_url = link_to_build(build)
+        @project = @build.project
+        @cucumber_output = cucumber_build_output(@build)
+        @rspec_output = rspec_build_output(@build)
+      end
+
+      def link_to_build(build)
+        @build_url = "http://ci.cloud.polarion.com/builds/#{build.to_param}"
+      end
+
+      def cucumber_build_output(build)
+        doc = Nokogiri::HTML(open(build.output_path('cucumber')))
+        failed_html_results(doc, ".step.failed")
+      end 
+
+      def rspec_build_output(build)
+        doc = Nokogiri::HTML(open(build.output_path('rspec')))
+        failed_html_results(doc, "dd.failed")
+      end
+
+      def failed_html_results(doc, failure_selector)
+        output = ""
+        added = []
+        output << doc.css("#summary").to_s
+        doc.css(failure_selector).each do |step|
+          container = step.parent.parent
+          unless added.include?(container)
+            output << container.to_s
+            added << container
+          end
+        end
+        output
+      end
+
     end
   end
 end
